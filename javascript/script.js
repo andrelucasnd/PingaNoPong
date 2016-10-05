0532a7ffc36f3d7c2183c3d8b9c2a1d742642f0c
andrelucasnd <andrelucasnd@gmail.com>
2016-10-05 10:15:04 -0300

    Criação das variáveis do canvas, context e das teclas (teclas do teclado) no script

---
+++ javascript/script.js
@@ -0,0 +1,4 @@
+var canvas = document.getElementById("mycanvas"); //adquire o elemento canvas do html
+var ctx = canvas.getContext("2d"); //define o contexto bidimensional do canvas
+
+var teclas = {}; //teclas do teclado
