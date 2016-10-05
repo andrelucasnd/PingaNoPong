a2b60a20f2c4b84a586bcd5334adcd8a9dc8e8e5
andrelucasnd <andrelucasnd@gmail.com>
2016-10-05 10:16:50 -0300

    Criação dos objetos presentes no jogo: barra da esquerda, barra da direita e bola

--- javascript/script.js
+++ javascript/script.js
@@ -2,3 +2,34 @@ var canvas = document.getElementById("mycanvas"); //adquire o elemento canvas do
 var ctx = canvas.getContext("2d"); //define o contexto bidimensional do canvas
 
 var teclas = {}; //teclas do teclado
+
+var esquerda = { //barra da esquerda
+  x: 10,
+  y: canvas.height / 2 - 60,
+  altura: 120,
+  largura: 30,
+  diry: 0,
+  score: 0,
+  speed: 10
+};
+
+var direita = { //barra da direita
+  x: canvas.width - 40,
+  y: canvas.height / 2 - 60,
+  altura: 120,
+  largura: 30,
+  diry: 0,
+  score: 0,
+  speed: 10
+};
+
+var bola = { //bola
+  x: canvas.width / 2 - 15,
+  y: canvas.height / 2 - 15,
+  altura: 30,
+  largura: 30,
+  dirx: -1,
+  diry: 1,
+  mod: 0,
+  speed: 1
+};
